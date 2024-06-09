27bfec0829736dfc8ebf7c952165b4c230f2064e
Karen Dantas <karennobre74@gmail.com>
2024-06-09 17:48:28 -0300

    feat: create prsima conection with mongo db

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const users = [{nome: "karen", idade: 20}];
 
 app.get('/', (req, res) => {
-    res.send('oi')
-})
+    res.send('oi');
+});
+
+app.post('/users', (req, res) => {
+    req.body = JSON.stringify(users)
+
+
+    res.status(201).send('post hihi');
+});
+
 app.get('/users', (req, res) => {
-    res.send('ok')
+    res.status(200).json(users)
 })
 
 app.listen(3000)
+
