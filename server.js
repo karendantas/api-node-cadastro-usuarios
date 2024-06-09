0a1eb3f58a025444e18ecdd3cb9323db55e68366
Karen Dantas <karennobre74@gmail.com>
2024-06-09 18:42:47 -0300

    feat: create api routes

--- server.js
+++ server.js
@@ -1,24 +1,80 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = [{nome: "karen", idade: 20}];
 
 app.get('/', (req, res) => {
     res.send('oi');
 });
 
-app.post('/users', (req, res) => {
-    req.body = JSON.stringify(users)
-
-
-    res.status(201).send('post hihi');
+//criando usuário
+app.post('/users', async (req, res) => {
+    
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+    res.status(201).send({message: `Usuário ${req.body.name} foi criado!`});
 });
 
-app.get('/users', (req, res) => {
-    res.status(200).json(users)
+//modificando usuario 
+app.put('/users/:id', async (req, res) => {
+    
+    await prisma.user.update({
+        where : {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+    res.status(201).send(req.body);
+});
+
+//deletando usuarios
+
+app.delete('/users/:id', async (req, res) => {
+    
+    await prisma.user.delete({
+        where : {
+            id: req.params.id
+        }
+    })
+    res.status(201).send({message: 'Usuário deletado!'});
+});
+
+
+//pegando todos os usuários
+app.get('/users', async (req, res) => {
+
+    let users = []
+    if(req.query){
+        users = await (prisma.user.findMany(
+            {
+                where: {
+                    name: req.query.name,
+                    email: req.query.email,
+                    age: req.query.age
+                }
+            }
+        ))
+    }else{
+        users = await prisma.user.findMany();
+    }
+   
+
+    res.status(200).json(users);
 })
+
 
 app.listen(3000)
 
